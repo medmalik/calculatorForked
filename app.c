0dfd3fab46359f3f1b3b442a4cadac9155d7b905
AboumalikAhmed <aboumalikahmed25@gmail.com>
2025-09-18 16:07:13 +0100

    resolve merge conflicts

--- app.c
+++ app.c
@@ -14,15 +14,7 @@ int main()
     printf("Enter the second number: ");
     scanf("%f",&number2);
     sum = (number1 + number2);
-    multiply = (number1 * number2);
-    divide = (number1 / number2);
-    subtract = (number1 - number2); 
-    printf("The sum of %.2f and %.2f is: %.2f\n",number1,number2,sum);
-    printf("The multiply of %.2f and %.2f is: %.2f\n",number1,number2,multiply);
-    printf("The divide of %.2f and %.2f is: %.2f\n",number1,number2,divide);
-    printf("The subtract of %.2f and %.2f is: %.2f\n",number1,number2,subtract);    
-
-
+ 
 
 
 
